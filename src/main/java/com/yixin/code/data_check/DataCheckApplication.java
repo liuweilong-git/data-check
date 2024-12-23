f3c8a16eb0dd175d2bab9049e4491a3085713065
学无止境 <7536456+CSaCA@user.noreply.gitee.com>
2024-12-23 23:29:51 +0800

    初始化

---
+++ src/main/java/com/yixin/code/data_check/DataCheckApplication.java
@@ -0,0 +1,13 @@
+package com.yixin.code.data_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yixin/code/data_check/DataCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yixin.code.data_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
